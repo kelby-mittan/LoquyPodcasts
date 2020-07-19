7390126e18703d0538040e5ebff1bec38d3a3eb5
Kelby Mittan <kelbymittan@pursuit.org>
2020-07-19 16:07:54 -0400

    basic views setup

---
+++ LoquyApp/LoquyApp/Views/EpisodeDetailView.swift
@@ -0,0 +1,21 @@
+//
+//  EpisodeDetailView.swift
+//  LoquyApp
+//
+//  Created by Kelby Mittan on 7/19/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import SwiftUI
+
+struct EpisodeDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EpisodeDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        EpisodeDetailView()
+    }
+}
