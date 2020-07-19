f9e8077b57a8c4d6cc98d29d1aa783c865b213a9
Kelby Mittan <kelbymittan@pursuit.org>
2020-07-19 16:03:41 -0400

    some files added

--- LoquyApp/LoquyApp/Supporting Files/SceneDelegate.swift
+++ LoquyApp/LoquyApp/Supporting Files/SceneDelegate.swift
@@ -24,12 +24,12 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
         // Create the SwiftUI view and set the context as the value for the managedObjectContext environment keyPath.
         // Add `@Environment(\.managedObjectContext)` in the views that will need the context.
-        let contentView = ContentView().environment(\.managedObjectContext, context)
+        let browseView = BrowseView().environment(\.managedObjectContext, context)
 
         // Use a UIHostingController as window root view controller.
         if let windowScene = scene as? UIWindowScene {
             let window = UIWindow(windowScene: windowScene)
-            window.rootViewController = UIHostingController(rootView: contentView)
+            window.rootViewController = UIHostingController(rootView: browseView)
             self.window = window
             window.makeKeyAndVisible()
         }
@@ -69,3 +69,9 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
 }
 
+
+struct SceneDelegate_Previews: PreviewProvider {
+    static var previews: some View {
+        /*@START_MENU_TOKEN@*/Text("Hello, World!")/*@END_MENU_TOKEN@*/
+    }
+}
---
+++ LoquyApp/LoquyApp/Views/EpisodesView.swift
@@ -0,0 +1,21 @@
+//
+//  EpisodesView.swift
+//  LoquyApp
+//
+//  Created by Kelby Mittan on 7/19/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import SwiftUI
+
+struct EpisodesView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct EpisodesView_Previews: PreviewProvider {
+    static var previews: some View {
+        EpisodesView()
+    }
+}
