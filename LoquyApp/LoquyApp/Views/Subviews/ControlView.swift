ba12e5e6540b30b0f1a7cd0906fc44a6eb73b54e
Kelby Mittan <kelbymittan@pursuit.org>
2020-08-23 20:31:58 -0400

    seek 15 sec

--- LoquyApp/LoquyApp/Views/Subviews/ControlView.swift
+++ LoquyApp/LoquyApp/Views/Subviews/ControlView.swift
@@ -7,15 +7,185 @@
 //
 
 import SwiftUI
+import AVKit
+import MediaPlayer
 
 struct ControlView: View {
+    
+    let episode: Episode
+    
+    @State var width : CGFloat = 30
+    @State var playing = true
+    @State var isFirstPlay = false
+    
+    let player: AVPlayer = {
+        let avPlayer = AVPlayer()
+        avPlayer.automaticallyWaitsToMinimizeStalling = false
+        return avPlayer
+    }()
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        VStack {
+            
+            ZStack(alignment: .leading) {
+                
+                Capsule().fill(Color.gray.opacity(0.2)).frame(height: 10)
+                    .padding([.top,.leading,.trailing])
+                
+                Capsule().fill(Color.blue).frame(width: self.width, height: 8)
+                    .gesture(DragGesture()
+                        .onChanged({ (value) in
+                            
+                            let x = value.location.x
+                            let maxVal = UIScreen.main.bounds.width - 10
+                            let minVal: CGFloat = 10
+                            
+                            if x < minVal {
+                                self.width = minVal
+                            } else if x > maxVal {
+                                self.width = maxVal
+                            } else {
+                                self.width = x
+                            }
+                            
+                            print("width val is : \(self.width)")
+                            
+                            
+                            
+                        }).onEnded({ (value) in
+                            
+                            let x = value.location.x
+                            
+                            let screen = UIScreen.main.bounds.width - 30
+                            
+                            //                            let percent = x / screen
+                            
+                            let percentage = x
+                            guard let duration = self.player.currentItem?.duration else { return }
+                            let durationInSeconds = CMTimeGetSeconds(duration)
+                            let seekTimeInSeconds = Float64(percentage) * durationInSeconds
+                            let seekTime = CMTimeMakeWithSeconds(seekTimeInSeconds, preferredTimescale: 1)
+                            
+                            MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPNowPlayingInfoPropertyElapsedPlaybackTime] = seekTimeInSeconds
+                            
+                            self.player.seek(to: seekTime)
+                            
+                            print(seekTime)
+                            
+                            //                            let x = value.location.x
+                            //
+                            //                            let screen = UIScreen.main.bounds.width - 30
+                            //
+                            //                            let percent = x / screen
+                        }))
+                    .padding([.top,.leading,.trailing])
+                
+            }
+            
+            HStack {
+                Text(observePlayerCurrentTime().currentTime)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+                Spacer()
+                Text(observePlayerCurrentTime().durationTime)
+                    .font(.caption)
+                    .foregroundColor(.secondary)
+            }
+            .padding([.leading,.trailing])
+            
+            HStack(spacing: UIScreen.main.bounds.width / 5 - 10) {
+                
+                Button(action: {
+                    self.seekToCurrentTime(delta: -15)
+                }) {
+                    Image(systemName: "gobackward.15").font(.largeTitle)
+                }
+                
+                Button(action: {
+                    self.playing.toggle()
+                    self.playing ? self.player.play() : self.player.pause()
+                    
+                }) {
+                    Image(systemName: self.playing ? "pause.fill" : "play.fill").font(.largeTitle)
+                }
+                
+                Button(action: {
+                    self.seekToCurrentTime(delta: 15)
+                    
+                }) {
+                    
+                    Image(systemName: "goforward.15").font(.largeTitle)
+                    
+                }
+                
+            }
+            .padding(.top,25)
+        }.onAppear {
+            self.playEpisode()
+            
+        }
     }
-}
-
-struct ControlView_Previews: PreviewProvider {
-    static var previews: some View {
-        ControlView()
+    
+    private func playEpisode() {
+        if episode.fileUrl != nil {
+            playEpisodeUsingFileUrl()
+        } else {
+            print("Trying to play episode at url:", episode.streamUrl)
+            
+            guard let url = URL(string: episode.streamUrl) else { return }
+            let playerItem = AVPlayerItem(url: url)
+            player.replaceCurrentItem(with: playerItem)
+            player.play()
+        }
+    }
+    
+    private func setupAudioSession() {
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+        } catch let sessionErr {
+            print("Failed to activate session:", sessionErr)
+        }
+    }
+    
+    private func playEpisodeUsingFileUrl() {
+        print("Attempt to play episode with file url:", episode.fileUrl ?? "")
+        
+        guard let fileURL = URL(string: episode.fileUrl ?? "") else { return }
+        let fileName = fileURL.lastPathComponent
+        
+        guard var trueLocation = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else { return }
+        
+        trueLocation.appendPathComponent(fileName)
+        print("True Location of episode:", trueLocation.absoluteString)
+        let playerItem = AVPlayerItem(url: trueLocation)
+        player.replaceCurrentItem(with: playerItem)
+        player.play()
+    }
+    
+//    private func setupElapsedTime(playbackRate: Float) {
+//        let elapsedTime = CMTimeGetSeconds(player.currentTime())
+//        MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPNowPlayingInfoPropertyElapsedPlaybackTime] = elapsedTime
+//        MPNowPlayingInfoCenter.default().nowPlayingInfo?[MPNowPlayingInfoPropertyPlaybackRate] = playbackRate
+//    }
+    
+    private func observePlayerCurrentTime() -> (currentTime: String, durationTime: String) {
+        let interval = CMTimeMake(value: 1, timescale: 2)
+        var currentTimeLabel = ""
+        var durationLabel = ""
+        player.addPeriodicTimeObserver(forInterval: interval, queue: .main) { (time) in
+            currentTimeLabel = time.toDisplayString()
+            let durationTime = self.player.currentItem?.duration
+            guard let dt = durationTime else { return }
+            durationLabel = dt.toDisplayString()
+        }
+        return (currentTimeLabel,durationLabel)
+    }
+    
+    private func seekToCurrentTime(delta: Int64) {
+        let fifteenSeconds = CMTimeMake(value: delta, timescale: 1)
+        let seekTime = CMTimeAdd(player.currentTime(), fifteenSeconds)
+        player.seek(to: seekTime)
     }
 }
