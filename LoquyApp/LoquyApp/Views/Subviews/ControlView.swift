77383e6a4de5cdd3c0e356c1399eb25deb87910e
Kelby Mittan <kelbymittan@pursuit.org>
2020-08-23 20:13:38 -0400

    organizing and playing on loading view

---
+++ LoquyApp/LoquyApp/Views/Subviews/ControlView.swift
@@ -0,0 +1,21 @@
+//
+//  ControlView.swift
+//  LoquyApp
+//
+//  Created by Kelby Mittan on 8/23/20.
+//  Copyright © 2020 Kelby Mittan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ControlView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ControlView_Previews: PreviewProvider {
+    static var previews: some View {
+        ControlView()
+    }
+}
