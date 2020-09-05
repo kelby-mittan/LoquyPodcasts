43914122c90b0a1a128733f1af29e39e3c8dc03d
Kelby Mittan <kelbymittan@pursuit.org>
2020-09-05 12:58:11 -0400

    ray w comp layout

--- LoquyApp/LoquyApp/Controllers+Cells/FavoritesViewController.swift
+++ LoquyApp/LoquyApp/Controllers+Cells/FavoritesViewController.swift
@@ -49,24 +49,110 @@ class FavoritesViewController: UIViewController {
     
     private func createLayout() -> UICollectionViewLayout {
         let layout = UICollectionViewCompositionalLayout { (sectionIndex, layoutEnvironment) -> NSCollectionLayoutSection? in
-            // item
-            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
-            let item = NSCollectionLayoutItem(layoutSize: itemSize)
-            let itemSpacing: CGFloat = 5 // points
-            item.contentInsets = NSDirectionalEdgeInsets(top: itemSpacing, leading: itemSpacing, bottom: itemSpacing, trailing: itemSpacing)
-            
-            // group (leadingGroup, trailingGroup, nestedGroup)
-            let innerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.50), heightDimension: .fractionalHeight(1.0))
-            
-            let leadingGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: 6)
-            
-            let trailingGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: 4)
-            
-            let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(1000))
-            
-            let nestedGroup = NSCollectionLayoutGroup.horizontal(layoutSize: nestedGroupSize, subitems: [leadingGroup, trailingGroup])
+//            // item
+//            let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .fractionalHeight(1.0))
+//            let item = NSCollectionLayoutItem(layoutSize: itemSize)
+//            let itemSpacing: CGFloat = 5 // points
+//            item.contentInsets = NSDirectionalEdgeInsets(top: itemSpacing, leading: itemSpacing, bottom: itemSpacing, trailing: itemSpacing)
+//
+//            // group (leadingGroup, trailingGroup, nestedGroup)
+//            let innerGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.50), heightDimension: .fractionalHeight(1.0))
+//
+//            let leadingGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: 6)
+//
+//            let trailingGroup = NSCollectionLayoutGroup.vertical(layoutSize: innerGroupSize, subitem: item, count: 4)
+//
+//            let nestedGroupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1.0), heightDimension: .absolute(1000))
+//
+//            let nestedGroup = NSCollectionLayoutGroup.horizontal(layoutSize: nestedGroupSize, subitems: [leadingGroup, trailingGroup])
             
             // section
+//            let section = NSCollectionLayoutSection(group: nestedGroup)
+            
+            
+            let fullArtItem = NSCollectionLayoutItem(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(2/3)))
+
+            fullArtItem.contentInsets = NSDirectionalEdgeInsets(
+              top: 2,
+              leading: 2,
+              bottom: 2,
+              trailing: 2)
+            
+            let mainItem = NSCollectionLayoutItem(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(2/3),
+                heightDimension: .fractionalHeight(1.0)))
+
+            mainItem.contentInsets = NSDirectionalEdgeInsets(
+              top: 2,
+              leading: 2,
+              bottom: 2,
+              trailing: 2)
+
+            // 2
+            let pairItem = NSCollectionLayoutItem(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalHeight(0.5)))
+
+            pairItem.contentInsets = NSDirectionalEdgeInsets(
+              top: 2,
+              leading: 2,
+              bottom: 2,
+              trailing: 2)
+
+            let trailingGroup = NSCollectionLayoutGroup.vertical(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1/3),
+                heightDimension: .fractionalHeight(1.0)),
+              subitem: pairItem,
+              count: 2)
+
+            // 1
+            let mainWithPairGroup = NSCollectionLayoutGroup.horizontal(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(4/9)),
+              subitems: [mainItem, trailingGroup])
+            
+            let tripletItem = NSCollectionLayoutItem(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1/3),
+                heightDimension: .fractionalHeight(1.0)))
+
+            tripletItem.contentInsets = NSDirectionalEdgeInsets(
+              top: 2,
+              leading: 2,
+              bottom: 2,
+              trailing: 2)
+
+            let tripletGroup = NSCollectionLayoutGroup.horizontal(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(2/9)),
+              subitems: [tripletItem, tripletItem, tripletItem])
+            
+            let mainWithPairReversedGroup = NSCollectionLayoutGroup.horizontal(
+            layoutSize: NSCollectionLayoutSize(
+              widthDimension: .fractionalWidth(1.0),
+              heightDimension: .fractionalWidth(4/9)),
+            subitems: [trailingGroup, mainItem])
+            
+            let nestedGroup = NSCollectionLayoutGroup.vertical(
+              layoutSize: NSCollectionLayoutSize(
+                widthDimension: .fractionalWidth(1.0),
+                heightDimension: .fractionalWidth(16/9)),
+              subitems: [
+                fullArtItem,
+                mainWithPairGroup,
+                tripletGroup,
+                mainWithPairReversedGroup
+              ]
+            )
+
             let section = NSCollectionLayoutSection(group: nestedGroup)
             
             return section
