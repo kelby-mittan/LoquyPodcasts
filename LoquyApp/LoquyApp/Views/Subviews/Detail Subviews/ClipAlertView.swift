8774facadd7f7c7589b8f2dd684d1dec857f6c93
Kelby Mittan <kelbymittan@pursuit.org>
2020-09-15 18:38:09 -0400

    have the picker view working

--- LoquyApp/LoquyApp/Views/Subviews/Detail Subviews/ClipAlertView.swift
+++ LoquyApp/LoquyApp/Views/Subviews/Detail Subviews/ClipAlertView.swift
@@ -10,8 +10,12 @@ import SwiftUI
 import AVKit
 
 struct ClipAlertView: View {
+    
+    @State var selected = ""
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        CustomPicker(selected: self.$selected, currentTime: "00:57:20")
     }
 }
 
@@ -23,6 +27,18 @@ struct ClipAlertView_Previews: PreviewProvider {
 
 struct CustomPicker: UIViewRepresentable {
     
+    @Binding var selected : String
+    
+    var currentTime: String
+
+//    init(time: String) {
+//        currentTime = time
+////        selected = selected1
+//    }
+    
+    func makeCoordinator() -> CustomPicker.Coordinator {
+        return CustomPicker.Coordinator(theParent: self, time: currentTime)
+    }
     
     func makeUIView(context: UIViewRepresentableContext<CustomPicker>) -> UIPickerView {
         
@@ -39,9 +55,11 @@ struct CustomPicker: UIViewRepresentable {
     class Coordinator: NSObject, UIPickerViewDelegate, UIPickerViewDataSource {
         
         var parent: CustomPicker
+        var currentTime: String
         
-        init(theParent: CustomPicker) {
+        init(theParent: CustomPicker, time: String) {
             parent = theParent
+            currentTime = time
         }
         
         
@@ -50,22 +68,58 @@ struct CustomPicker: UIViewRepresentable {
         }
         
         func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
-            <#code#>
+            return getEndTimeArr(currentTime).count
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+            return getEndTimeArr(currentTime)[row]
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, viewForRow row: Int, forComponent component: Int, reusing view: UIView?) -> UIView {
+            let view = UIView(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width - 200, height: 60))
+            view.backgroundColor = .systemPurple
+            
+            let label = UILabel(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: view.bounds.height))
+            label.text = getEndTimeArr(currentTime)[row]
+            label.textColor = .white
+            label.textAlignment = .center
+            label.font = UIFont.systemFont(ofSize: 20, weight: .bold)
+            
+            view.clipsToBounds = true
+            view.layer.cornerRadius = view.bounds.height / 2
+            view.addSubview(label)
+            return view
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, widthForComponent component: Int) -> CGFloat {
+            return UIScreen.main.bounds.width - 200
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat {
+            return 60
+        }
+        
+        func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+            self.parent.selected = getEndTimeArr(currentTime)[row]
+        }
+        
+        func getEndTimeArr(_ currentTime: String = "00:00:00") -> [String] {
+            var endTimeArr = [String]()
+            
+            var counterIncrements = 29.0
+            
+            for _ in 0..<9{
+                let sec = currentTime.toSecDouble() + counterIncrements
+                let cmTime = CMTime(seconds: sec, preferredTimescale: 1)
+                var timeStr = cmTime.toDisplayString()
+//                timeStr = timeStr.replacingOccurrences(of: "00:", with: "")
+                endTimeArr.append(timeStr)
+                counterIncrements += 29.0
+            }
+            
+            return endTimeArr
         }
     }
     
-    func getEndTimeArr(_ currentTime: String) -> [String] {
-        var endTimeArr = [String]()
-        
-        var counterIncrements = 30.0
-        
-        for _ in 0..<9{
-            let sec = currentTime.toSecDouble() + counterIncrements
-            let cmTime = CMTime(seconds: sec, preferredTimescale: 1)
-            endTimeArr.append(cmTime.toDisplayString())
-            counterIncrements += 30.0
-        }
-        
-        return endTimeArr
-    }
+    
 }
