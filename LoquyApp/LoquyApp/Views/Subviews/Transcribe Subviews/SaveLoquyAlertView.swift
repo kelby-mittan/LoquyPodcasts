0195b19e328831afe3cd7deeff1327bce61ea319
Kelby Mittan <kelbymittan@pursuit.org>
2020-09-29 17:03:25 -0400

    notification for saved transcription

--- LoquyApp/LoquyApp/Views/Subviews/Transcribe Subviews/SaveLoquyAlertView.swift
+++ LoquyApp/LoquyApp/Views/Subviews/Transcribe Subviews/SaveLoquyAlertView.swift
@@ -11,16 +11,15 @@ import SwiftUI
 struct SaveLoquyAlertView: View {
     
     @Binding var showAlert: Bool
-//    @Binding var time: String
+    @Binding var notificationShown: Bool
+    @Binding var message: String
     
     @ObservedObject var networkManager: NetworkingManager
     
     let audioClip: AudioClip
-    
     let transcription: String
     
     @State var timeStamps = [TimeStamp]()
-    
     @State var titleText = ""
     
     
@@ -33,7 +32,7 @@ struct SaveLoquyAlertView: View {
                 HStack {
                     Spacer()
                     Button(action: {
-                        self.showAlert.toggle()
+                        showAlert.toggle()
                         
                     }, label: {
                         Image(systemName: "xmark")
@@ -58,39 +57,17 @@ struct SaveLoquyAlertView: View {
                     .textFieldStyle(SuperCustomTextFieldStyle())
                 
                 Button(action: {
-                    
                     showAlert.toggle()
-                    
-                    var id = 1
-
-                    let filteredLoquys = networkManager.loquys.filter { $0.audioClip.episode.imageUrl == audioClip.episode.imageUrl }
-
-                    if !filteredLoquys.isEmpty {
-                        id = filteredLoquys.count + 1
-                    }
-
-                    print(filteredLoquys.count)
-                    print(id)
-
-                    let newLoquy = Loquy(idInCollection: id, title: titleText, transcription: transcription, audioClip: audioClip)
-
-                    do {
-                        try Persistence.loquys.createItem(newLoquy)
-                    } catch {
-                        print("problem creating loquy")
-                    }
-
-                    networkManager.loadLoquys()
-                    
-                    
+                    saveLoquyToList()
+                    handleNotification()
                 }) {
-                        Text("save")
-                            .fontWeight(.bold)
-                            .frame(width: 120,height: 40)
-                            .foregroundColor(Color.purple)
-                            .background(NeoButtonView())
-                            .clipShape(Capsule())
-                            .padding()
+                    Text("save")
+                        .fontWeight(.bold)
+                        .frame(width: 120,height: 40)
+                        .foregroundColor(Color.purple)
+                        .background(NeoButtonView())
+                        .clipShape(Capsule())
+                        .padding()
                 }
                 Spacer()
             }
@@ -108,5 +85,31 @@ struct SaveLoquyAlertView: View {
         .cornerRadius(20)
         .shadow(color: Color(#colorLiteral(red: 0.748958528, green: 0.7358155847, blue: 0.9863374829, alpha: 1)), radius: 16, x: 10, y: 10)
         .shadow(color: Color(.white), radius: 16, x: -12, y: -12)
+    }
+    
+    func saveLoquyToList() {
+        var id = 1
+        let filteredLoquys = networkManager.loquys.filter { $0.audioClip.episode.imageUrl == audioClip.episode.imageUrl }
+        
+        if !filteredLoquys.isEmpty {
+            id = filteredLoquys.count + 1
+        }
+        let newLoquy = Loquy(idInCollection: id, title: titleText, transcription: transcription, audioClip: audioClip)
+        do {
+            try Persistence.loquys.createItem(newLoquy)
+        } catch {
+            print("problem creating loquy")
+        }
+
+        networkManager.loadLoquys()
+    }
+    
+    func handleNotification() {
+        notificationShown = true
+        message = "loquy saved"
+        
+        Timer.scheduledTimer(withTimeInterval: 2.5, repeats: false) { (value) in
+            notificationShown = false
+        }
     }
 }
--- LoquyApp/LoquyApp/Views/TranscribeView.swift
+++ LoquyApp/LoquyApp/Views/TranscribeView.swift
@@ -23,8 +23,10 @@ struct TranscribeView: View {
     @State var title: String = ""
     @State var isTranscribing = false
     @State var saveText = "transcribe"
-    
     @State var image = RemoteImage(url: "")
+    
+    @State var showNotification = false
+    @State var notificationMessage = ""
     
     let player = Player.shared.player
     
@@ -33,201 +35,209 @@ struct TranscribeView: View {
     @State var showAlert = false
     
     var body: some View {
-        ScrollView(.vertical, showsIndicators: false) {
-            
-            Text(audioClip.title)
-                .fontWeight(.heavy)
-                .foregroundColor(Color(.label))
-                .font(.title)
-                .padding(.bottom,6)
-            
-            NavigationLink(destination: EpisodeDetailView(episode: audioClip.episode, artwork: audioClip.episode.imageUrl ?? "")) {
+        ZStack {
+            ScrollView(.vertical, showsIndicators: false) {
                 
-                Text(audioClip.episode.title)
+                Text(audioClip.title)
                     .fontWeight(.heavy)
-                    .foregroundColor(Color.purple)
-                    .font(.headline)
-                    .padding([.leading,.trailing])
-            }.onTapGesture(perform: {
-                playing = false
-            })
-            
-            Text(audioClip.startTime + " - " + audioClip.endTime)
-                .fontWeight(.heavy)
-                .foregroundColor(Color(.label))
-                .font(.subheadline)
-                .padding(.top,4)
-            
-            HStack {
-                image
-                    .frame(width: 100, height: 100)
-                    .cornerRadius(6)
+                    .foregroundColor(Color(.label))
+                    .font(.title)
+                    .padding(.bottom,6)
                 
-                Spacer()
-                
-                Button(action: {
+                NavigationLink(destination: EpisodeDetailView(episode: audioClip.episode, artwork: audioClip.episode.imageUrl ?? "")) {
                     
-                    playing.toggle()
-                    playing ? player.play() : player.pause()
-                }) {
-                    ZStack {
-                        NeoButtonView()
-                        Image(systemName: playing ? "pause.fill" : "play.fill").font(.largeTitle)
-                            .foregroundColor(.purple)
-                            
-                    }
-                    .background(NeoButtonView())
-                    .frame(width: 64, height: 64)
-                    .shadow(color: Color(#colorLiteral(red: 0.748958528, green: 0.7358155847, blue: 0.9863374829, alpha: 1)), radius: 8, x: 6, y: 6)
-                    .shadow(color: Color(.white), radius: 10, x: -6, y: -6)
-                    .clipShape(Capsule())
-                    .padding(.trailing)
-                }
-            }
-            .padding([.leading,.trailing,.bottom])
-            
-            ZStack(alignment: .leading) {
-                
-                Capsule().fill(Color.gray.opacity(0.2)).frame(height: 10)
-                
-                Capsule().fill(Color.purple).frame(width: width, height: 8)
-                    .gesture(DragGesture()
-                        .onChanged({ (value) in
-                            
-                            player.pause()
-                            let x = value.location.x
-                            let maxVal = UIScreen.main.bounds.width - 30
-                            let minVal: CGFloat = 10
-                            
-                            if x < minVal {
-                                width = minVal
-                            } else if x > maxVal {
-                                width = maxVal
-                            } else {
-                                width = x
-                            }
-                            currentTime = Player.capsuleDragged(value.location.x).toDisplayString()
-                            print("width val is : \(width)")
-                            
-                        }).onEnded({ (value) in
-                            player.seek(to: Player.capsuleDragged(value.location.x))
-                            player.play()
-                            playing = true
-                        }))
-            }.padding([.leading,.trailing])
-            
-            HStack {
-                Text(currentTime)
-                    .font(.caption)
-                    .foregroundColor(.secondary)
-                    .padding(.leading, 4)
-                Spacer()
-                Text(getCurrentPlayerTime().durationTime)
-                    .font(.caption)
-                    .foregroundColor(.secondary)
-                    .padding(.trailing, 4)
-            }
-            
-            Group {
-                if isTranscribing {
-                    
-                    VStack(alignment: .leading) {
-                        Text("Your Loquy...")
-                            .font(.title)
-                            .fontWeight(.heavy)
-            
-                        MultilineTextField("", text: $transcription, onCommit: {
-                            print("Final text: \(transcription)")
-                            player.pause()
-                            DispatchQueue.main.async {
-                                playing = false
-                            }
-                        })
-                    }.padding()
-                }
-            }
-            Button(action: {
-
-                isTranscribing.toggle()
-                
-                if isTranscribing {
-                    getTranscriptionOfClippedFile()
-                    saveText = "save loquy"
-                    
-                } else {
-                    saveText = "transcribe"
-                    player.pause()
+                    Text(audioClip.episode.title)
+                        .fontWeight(.heavy)
+                        .foregroundColor(Color.purple)
+                        .font(.headline)
+                        .padding([.leading,.trailing])
+                }.onTapGesture(perform: {
                     playing = false
-                    print(transcription)
-                    
-//                    var id = 1
-//
-//                    let filteredLoquys = networkManager.loquys.filter { $0.audioClip.episode.imageUrl == audioClip.episode.imageUrl }
-//
-//                    if !filteredLoquys.isEmpty {
-//                        id = filteredLoquys.count + 1
-//                    }
-//
-//                    print(filteredLoquys.count)
-//                    print(id)
-//
-//                    let newLoquy = Loquy(idInCollection: id, title: title, transcription: transcription, audioClip: audioClip)
-//
-//                    do {
-//                        try Persistence.loquys.createItem(newLoquy)
-//                    } catch {
-//                        print("problem creating loquy")
-//                    }
-//
-//                    getLoquyTranscriptions()
-                    
-                    showAlert.toggle()
-                }
+                })
                 
-                
-            }) {
-                Text(saveText)
+                Text(audioClip.startTime + " - " + audioClip.endTime)
                     .fontWeight(.heavy)
-                    .padding()
-                    .frame(width: UIScreen.main.bounds.width - 88)
-                    .foregroundColor(.purple)
-                    .background(NeoButtonView())
-                    .clipShape(Capsule())
-                    .shadow(color: Color(#colorLiteral(red: 0.748958528, green: 0.7358155847, blue: 0.9863374829, alpha: 1)), radius: 16, x: 10, y: 10)
-                    .shadow(color: Color(.white), radius: 16, x: -12, y: -12)
-                    .padding()
-//                        Spacer()
-            }
-            
-            Spacer()
-            
-            if showAlert {
-                SaveLoquyAlertView(showAlert: $showAlert, networkManager: networkManager, audioClip: audioClip, transcription: transcription)
-                .offset(x: 0, y: -70)
-            }
-            
-        }.onAppear(perform: {
-            
-            image = RemoteImage(url: audioClip.episode.imageUrl ?? "")
-            getLoquyTranscriptions()
-            
-            guard let url = AudioTrim.loadUrlFromDiskWith(fileName: audioClip.episode.title + audioClip.startTime + ".m4a") else {
-                print(AudioTrim.loadUrlFromDiskWith(fileName: audioClip.episode.title + ".m4a") ?? "Couldn't Find MP3")
-                return
-            }
-
-            Player.playAudioClip(url: url)
-            playing = true
-            getCurrentPlayerTime()
-
-            Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (value) in
-                if playing {
-                    if player.currentItem?.duration.toDisplayString() != "--:--" && width > 0.0 {
-                        Player.getCapsuleWidth(width: &width, currentTime: currentTime)
+                    .foregroundColor(Color(.label))
+                    .font(.subheadline)
+                    .padding(.top,4)
+                
+                HStack {
+                    image
+                        .frame(width: 100, height: 100)
+                        .cornerRadius(6)
+                    
+                    Spacer()
+                    
+                    Button(action: {
+                        
+                        playing.toggle()
+                        playing ? player.play() : player.pause()
+                    }) {
+                        ZStack {
+                            NeoButtonView()
+                            Image(systemName: playing ? "pause.fill" : "play.fill").font(.largeTitle)
+                                .foregroundColor(.purple)
+                                
+                        }
+                        .background(NeoButtonView())
+                        .frame(width: 64, height: 64)
+                        .shadow(color: Color(#colorLiteral(red: 0.748958528, green: 0.7358155847, blue: 0.9863374829, alpha: 1)), radius: 8, x: 6, y: 6)
+                        .shadow(color: Color(.white), radius: 10, x: -6, y: -6)
+                        .clipShape(Capsule())
+                        .padding(.trailing)
                     }
                 }
-            }
+                .padding([.leading,.trailing,.bottom])
+                
+                ZStack(alignment: .leading) {
+                    
+                    Capsule().fill(Color.gray.opacity(0.2)).frame(height: 10)
+                    
+                    Capsule().fill(Color.purple).frame(width: width, height: 8)
+                        .gesture(DragGesture()
+                            .onChanged({ (value) in
+                                
+                                player.pause()
+                                let x = value.location.x
+                                let maxVal = UIScreen.main.bounds.width - 30
+                                let minVal: CGFloat = 10
+                                
+                                if x < minVal {
+                                    width = minVal
+                                } else if x > maxVal {
+                                    width = maxVal
+                                } else {
+                                    width = x
+                                }
+                                currentTime = Player.capsuleDragged(value.location.x).toDisplayString()
+                                print("width val is : \(width)")
+                                
+                            }).onEnded({ (value) in
+                                player.seek(to: Player.capsuleDragged(value.location.x))
+                                player.play()
+                                playing = true
+                            }))
+                }.padding([.leading,.trailing])
+                
+                HStack {
+                    Text(currentTime)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                        .padding(.leading, 4)
+                    Spacer()
+                    Text(getCurrentPlayerTime().durationTime)
+                        .font(.caption)
+                        .foregroundColor(.secondary)
+                        .padding(.trailing, 4)
+                }
+                
+                Group {
+                    if isTranscribing {
+                        
+                        VStack(alignment: .leading) {
+                            Text("Your Loquy...")
+                                .font(.title)
+                                .fontWeight(.heavy)
+                
+                            MultilineTextField("", text: $transcription, onCommit: {
+                                print("Final text: \(transcription)")
+                                player.pause()
+                                DispatchQueue.main.async {
+                                    playing = false
+                                }
+                            })
+                        }.padding()
+                    }
+                }
+                Button(action: {
+
+                    isTranscribing.toggle()
+                    
+                    if isTranscribing {
+                        getTranscriptionOfClippedFile()
+                        saveText = "save loquy"
+                        
+                    } else {
+                        saveText = "transcribe"
+                        player.pause()
+                        playing = false
+                        print(transcription)
+                        
+    //                    var id = 1
+    //
+    //                    let filteredLoquys = networkManager.loquys.filter { $0.audioClip.episode.imageUrl == audioClip.episode.imageUrl }
+    //
+    //                    if !filteredLoquys.isEmpty {
+    //                        id = filteredLoquys.count + 1
+    //                    }
+    //
+    //                    print(filteredLoquys.count)
+    //                    print(id)
+    //
+    //                    let newLoquy = Loquy(idInCollection: id, title: title, transcription: transcription, audioClip: audioClip)
+    //
+    //                    do {
+    //                        try Persistence.loquys.createItem(newLoquy)
+    //                    } catch {
+    //                        print("problem creating loquy")
+    //                    }
+    //
+    //                    getLoquyTranscriptions()
+                        
+                        showAlert.toggle()
+                    }
+                    
+                    
+                }) {
+                    Text(saveText)
+                        .fontWeight(.heavy)
+                        .padding()
+                        .frame(width: UIScreen.main.bounds.width - 88)
+                        .foregroundColor(.purple)
+                        .background(NeoButtonView())
+                        .clipShape(Capsule())
+                        .shadow(color: Color(#colorLiteral(red: 0.748958528, green: 0.7358155847, blue: 0.9863374829, alpha: 1)), radius: 16, x: 10, y: 10)
+                        .shadow(color: Color(.white), radius: 16, x: -12, y: -12)
+                        .padding()
+    //                        Spacer()
+                }
+                
+                Spacer()
+                
+                if showAlert {
+                    SaveLoquyAlertView(showAlert: $showAlert, notificationShown: $showNotification, message: $notificationMessage, networkManager: networkManager, audioClip: audioClip, transcription: transcription)
+                    .offset(x: 0, y: -70)
+                }
+                
+            }.onAppear(perform: {
+                
+                image = RemoteImage(url: audioClip.episode.imageUrl ?? "")
+                getLoquyTranscriptions()
+                
+                guard let url = AudioTrim.loadUrlFromDiskWith(fileName: audioClip.episode.title + audioClip.startTime + ".m4a") else {
+                    print(AudioTrim.loadUrlFromDiskWith(fileName: audioClip.episode.title + ".m4a") ?? "Couldn't Find MP3")
+                    return
+                }
+
+                Player.playAudioClip(url: url)
+                playing = true
+                getCurrentPlayerTime()
+
+                Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { (value) in
+                    if playing {
+                        if player.currentItem?.duration.toDisplayString() != "--:--" && width > 0.0 {
+                            Player.getCapsuleWidth(width: &width, currentTime: currentTime)
+                        }
+                    }
+                }
         })
+            
+            NotificationView(message: $notificationMessage)
+                .offset(y: showNotification ? -UIScreen.main.bounds.height/3 : -UIScreen.main.bounds.height)
+                .animation(.interpolatingSpring(mass: 1, stiffness: 100, damping: 12, initialVelocity: 0))
+        }
+        
+        
         
     }
     
