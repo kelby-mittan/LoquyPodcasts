68b83302973bdd67b68cf012ed88c4e930262c36
kelby-mittan <kelbymittan@pursuit.org>
2021-10-05 15:28:55 -0400

    working with a scroll view for nav link

--- LoquyApp/LoquyApp/Views/EpisodesView.swift
+++ LoquyApp/LoquyApp/Views/EpisodesView.swift
@@ -11,12 +11,12 @@ import SwiftUI
 @available(iOS 14.0, *)
 struct EpisodesView: View {
     
-    @EnvironmentObject var viewModel: ViewModel    
+    @EnvironmentObject var viewModel: ViewModel
     @State public var title: String
     public let podcastFeed: String?
     public let isSaved: Bool
     public let artWork: String
-
+    
     @State var episode: Episode?
     @State var domColor: UIColor?
     
@@ -27,72 +27,130 @@ struct EpisodesView: View {
                     .padding(.top,40)
                     .onAppear {
                         isSaved
-                            ? viewModel.episodes = viewModel.loadFavoriteEpisodes(&title)
-                            : viewModel.loadEpisodes(feedUrl: podcastFeed ?? RepText.empty)
+                        ? viewModel.episodes = viewModel.loadFavoriteEpisodes(&title)
+                        : viewModel.loadEpisodes(feedUrl: podcastFeed ?? RepText.empty)
                         UITableView.appearance().separatorStyle = .none
                     }
                 Spacer()
             }
             .navigationBarTitle(title)
         } else {
-            List(viewModel.episodes, id: \.self) { episode in
-                
-                ZStack {
-                    NavigationLink(destination: EpisodeDetailView(episode: episode,
-                                                                  artwork: artWork,
-                                                                  feedUrl: podcastFeed,
-                                                                  isDeepLink: false,
-                                                                  domColor: domColor ?? .lightGray)
-                                    .environmentObject(viewModel)) {
-                        EmptyView()
-                    }
-                    .padding()
-                    .hidden()
-                    
-                    ZStack {
-                        ZStack {
-                            Color(domColor ?? .systemBackground)
-                        }
-                        .cornerRadius(12)
-                        
-                        HStack {
+            
+            ScrollView {
+                ForEach(viewModel.episodes, id: \.self) { episode in
+//                    ZStack {
+                        NavigationLink(destination: EpisodeDetailView(episode: episode,
+                                                                      artwork: artWork,
+                                                                      feedUrl: podcastFeed,
+                                                                      isDeepLink: false,
+                                                                      domColor: domColor ?? .lightGray)
+                                        .environmentObject(viewModel)) {
                             
-                            RemoteImage(url: episode.imageUrl ?? RepText.empty)
-                                .frame(width: 120, height: 120)
-                                .cornerRadius(6)
-                                .padding(.vertical)
+//                            EmptyView()
                             
-                            VStack(alignment: .leading) {
+                            ZStack {
+                                Color(domColor ?? .systemBackground)
                                 
-                                Text(episode.pubDate.makeString())
-                                    .font(.headline)
-                                    .fontWeight(.heavy)
-                                    .foregroundColor(Color.white)
-                                    .padding(.bottom,4)
-                                
-                                Text(episode.title)
-                                    .font(.subheadline)
-                                    .foregroundColor(Color.white)
-                                    .fontWeight(.semibold)
-                                    .lineLimit(4)
-                                
-                                Spacer()
-                                
+                                HStack {
+                                    
+                                    RemoteImage(url: episode.imageUrl ?? RepText.empty)
+                                        .frame(width: 120, height: 120)
+                                        .cornerRadius(6)
+                                        .padding(.vertical)
+                                    
+                                    VStack(alignment: .leading) {
+                                        
+                                        Text(episode.pubDate.makeString())
+                                            .font(.headline)
+                                            .fontWeight(.heavy)
+                                            .foregroundColor(Color.white)
+                                            .padding(.bottom,4)
+                                        
+                                        Text(episode.title)
+                                            .font(.subheadline)
+                                            .foregroundColor(Color.white)
+                                            .fontWeight(.semibold)
+                                            .lineLimit(4)
+                                        
+                                        Spacer()
+                                        
+                                    }
+                                    .padding()
+                                }
+                                .padding()
                             }
-                            .padding()
+                            .cornerRadius(12)
                         }
+                                        .background(Color.yellow)
                         .padding()
+                        //                    .hidden()
                         
-                    }
-                    .cornerRadius(12)
+                        
+                        
+//                    }
                 }
-                
             }
+            
+            
+            
+            
+//            List(viewModel.episodes, id: \.self) { episode in
+//
+//                ZStack {
+//                    NavigationLink(destination: EpisodeDetailView(episode: episode,
+//                                                                  artwork: artWork,
+//                                                                  feedUrl: podcastFeed,
+//                                                                  isDeepLink: false,
+//                                                                  domColor: domColor ?? .lightGray)
+//                                    .environmentObject(viewModel)) {
+//
+//                        EmptyView()
+//                    }
+//                                    .background(Color.yellow)
+//                    .padding()
+//                    //                    .hidden()
+//
+//                    ZStack {
+//                        Color(domColor ?? .systemBackground)
+//
+//                        HStack {
+//
+//                            RemoteImage(url: episode.imageUrl ?? RepText.empty)
+//                                .frame(width: 120, height: 120)
+//                                .cornerRadius(6)
+//                                .padding(.vertical)
+//
+//                            VStack(alignment: .leading) {
+//
+//                                Text(episode.pubDate.makeString())
+//                                    .font(.headline)
+//                                    .fontWeight(.heavy)
+//                                    .foregroundColor(Color.white)
+//                                    .padding(.bottom,4)
+//
+//                                Text(episode.title)
+//                                    .font(.subheadline)
+//                                    .foregroundColor(Color.white)
+//                                    .fontWeight(.semibold)
+//                                    .lineLimit(4)
+//
+//                                Spacer()
+//
+//                            }
+//                            .padding()
+//                        }
+//                        .padding()
+//                    }
+//                    .cornerRadius(12)
+//
+//                }
+                                
+//            }
             .onAppear {
                 isSaved
-                    ? viewModel.episodes = viewModel.loadFavoriteEpisodes(&title)
-                    : viewModel.loadEpisodes(feedUrl: podcastFeed ?? RepText.empty)
-                      
+                ? viewModel.episodes = viewModel.loadFavoriteEpisodes(&title)
+                : viewModel.loadEpisodes(feedUrl: podcastFeed ?? RepText.empty)
+                
                 if let firstEpisodeImg = viewModel.episodes.first?.imageUrl {
                     viewModel.getDomColor(firstEpisodeImg) { clr in
                         if clr.isDark() {
